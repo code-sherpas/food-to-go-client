39722e414819d7478f0ba2050f7c5b2c7cb62c7e
ashikka <ashikagupta28@gmail.com>
2021-11-22 02:04:44 +0530

    fix: UI changes

--- App.tsx
+++ App.tsx
@@ -13,21 +13,29 @@ export default function App() {
         },
       ]}
     >
-      <View style={{ flex: 2, flexDirection: "column-reverse",  }}>
+      <View style={{ flex: 2 }}>
         <ImageBackground
           source={require("./assets/food.jpg")}
-          style={{ width: "100%", height: "100%" }}
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
         >
           <View
             style={{
               flexDirection: "row",
               alignItems: "center",
               justifyContent: "flex-start",
+              alignContent: "center",
+              marginBottom: 15
             }}
           >
-            <Image
-              source={require("./assets/icon.png")}
-              style={{ width: 30, height: 30, marginRight: 10 }}
+            <Icon
+              name="hamburger"
+              color="white"
+              style={{ marginHorizontal: 10, fontSize: 20 }}
             />
             <Text style={styles.heading}>Restaurant</Text>
           </View>
@@ -61,7 +69,7 @@ export default function App() {
         <Icon name="clock" style={{ marginRight: 10 }} />
         <Text>Today 15:00 - 16:30</Text>
       </View>
-      <div
+      <View
         style={{
           display: "flex",
           justifyContent: "center",
@@ -72,7 +80,7 @@ export default function App() {
         }}
       >
         <Text style={{ fontWeight: "bold" }}>Sale ends soon!</Text>
-      </div>
+      </View>
       <View
         style={{
           borderBottomColor: "#DCE0E1",
@@ -81,14 +89,22 @@ export default function App() {
           padding: 15,
           flexDirection: "row",
           justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        <div style={{ marginLeft: 5 }}>
+        <View
+          style={{
+            marginLeft: 5,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           <Icon name="map-marker-alt" color="#114D4D" />
-        </div>
-        <div
+        </View>
+        <View
           style={{
             height: 10,
+            width: 315,
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
@@ -102,10 +118,10 @@ export default function App() {
           <Text style={{ color: "grey", fontSize: 12, marginTop: 2 }}>
             More information about the store
           </Text>
-        </div>
-        <div style={{ padding: 20 }}>
+        </View>
+        <View style={{ padding: 20 }}>
           <Icon name="angle-right" color="#114D4D" />
-        </div>
+        </View>
       </View>
       <View
         style={{
@@ -129,21 +145,26 @@ export default function App() {
           Salva comida en Wonder Cookies y tu pack podra tener: una variedad de
           galletas.
         </Text>
-        <div
+        <View
           style={{
-            width: 125,
-            height: 20,
-            fontSize: 15,
+            width: 150,
+            height: 25,
             padding: 5,
             backgroundColor: "#BDE1DD",
-            borderRadius: "3px",
-            textAlign: "center",
+            borderRadius: 3,
             marginTop: 20,
             marginBottom: 10,
           }}
         >
-          Breads and Pastries
-        </div>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 13,
+            }}
+          >
+            Breads and Pastries
+          </Text>
+        </View>
       </View>
       <View
         style={{
@@ -153,12 +174,13 @@ export default function App() {
           padding: 15,
           flexDirection: "row",
           justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        <div style={{ marginLeft: 5 }}>
+        <View style={{ marginLeft: 5 }}>
           <Icon name="allergies" color="#114D4D" />
-        </div>
-        <div
+        </View>
+        <View
           style={{
             display: "flex",
             flexDirection: "row",
@@ -167,19 +189,27 @@ export default function App() {
           }}
         >
           <Text style={{ color: "#114D4D" }}>Ingredients and allergens</Text>
-        </div>
-        <div style={{ paddingRight: 20 }}>
+        </View>
+        <View style={{ paddingRight: 20 }}>
           <Icon name="angle-right" color="#114D4D" />
-        </div>
+        </View>
       </View>
       <View style={styles.bottomRating}>
         <Text>
           <b>WHAT ARE OTHER PEOPLE SAYING</b>
         </Text>
-        <div style={{ marginTop: 15 }}>
+        <View
+          style={{
+            marginTop: 15,
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Icon style={{ marginRight: 10 }} solid name="star" color="gold" />
           <Text style={styles.numberRating}>4.4 / 5.0</Text>
-        </div>
+        </View>
       </View>
     </View>
   );
@@ -207,8 +237,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   heading: {
-    fontSize: 20,
-    fontWeight: "900",
+    fontSize: 25,
+    fontWeight: "bold",
     color: "white",
   },
   container: {
