501ee6c01f0c3b1fa8053a574904a6634f919431
ashikka <ashikagupta28@gmail.com>
2021-12-20 19:40:10 +0530

    fix: background color

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { View, Text, StyleSheet, Button } from 'react-native';
 import ListofRestaurantView from "./views/listOfRestaurantView";
 import RestaurantView from "./views/restaurantView";
 
 export default function App() {
   return (
     <Provider store={store}>
+      <View style={styles.rootContainer}>
       {/* <RestaurantView /> */}
       <ListofRestaurantView/>
+      </View>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    backgroundColor: "#FCF9F2"
+  }
+})
