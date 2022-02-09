e0dc8c60f73e84c7d54b8b9584485be0667dcfb9
ashikka <ashikagupta28@gmail.com>
2022-02-09 21:29:48 +0530

    feat: Linking buttons

--- App.tsx
+++ App.tsx
@@ -5,14 +5,23 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import ListOfRestaurantView from "./views/listOfRestaurantView";
 import RestaurantView from "./views/restaurantView";
 import MapView from "./views/MapView";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
       <View style={styles.rootContainer}>
-        {/* <RestaurantView /> */}
-        <ListOfRestaurantView />
-        {/* <MapView /> */}
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={MapView} options={{ headerShown: false }} />
+            <Stack.Screen name="ListOfRestaurantView" component={ListOfRestaurantView} options={{ headerShown: false }} />
+            <Stack.Screen name="RestaurantView" component={RestaurantView} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
     </Provider>
   );
