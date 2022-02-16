20464364f6bef3951d54765d3f3005507c773748
ashikka <ashikagupta28@gmail.com>
2022-02-16 19:03:05 +0530

    fix: Response value

--- api/dealsResponse.ts
+++ api/dealsResponse.ts
@@ -16,7 +16,7 @@ const response = {
           "store_id": "2ef65b3b-8f4e-4288-8b58-998cb6bdab46",
           "type": "Magic Bag",
           "quantity_in_stock": 14,
-          "available_from": "2021-11-157T21:00:00Z",
+          "available_from": "2021-11-15T21:00:00Z",
           "available_until": "2021-11-16T21:00:00Z",
           "unitary_price_after_discount": {
             "value": 15,
